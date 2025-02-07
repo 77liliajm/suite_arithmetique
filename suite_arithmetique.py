cce1b787cbda82fb4bcf3322eecb382388b5c48e
77liliajm <liliajomaa@hotmail.fr>
2025-02-07 16:43:09 +0100

    Add files via upload

---
+++ suite_arithmetique.py
@@ -0,0 +1,23 @@
+# programme suite arithmetique : exercice 3 de mon cours d'algorithmique
+# but : conception d'un algorithme affichant les nb premiers termes de la suite et la somme des nb premiers termes de la suite
+# date : 07/02/2025
+# auteur : Lilia
+
+# saisie des valeurs de la suite : le premier terme (u0), la raison (r) et le nombre de terme à gérer (nb)
+uo = int(input("Entrez le premier terme de la suite = "))
+r = int(input("Entrez la raison de la suite = "))
+nb = int(input("Entrez le nombre de termes = "))
+
+# initialisation de terme (qui démarre à u0)
+terme = uo
+
+# boucle pour afficher les nb premiers termes de la suite
+for k in range (0, nb):
+    print("u", k, " = ", terme)
+    terme = terme + r
+
+# boucle pour afficher la somme des nb premiers terme de la suite
+somme = 0
+for i in range (1, nb+1):
+    somme = somme + i
+print("la somme des nb premiers termes de la suite est égal à " + str(somme))
